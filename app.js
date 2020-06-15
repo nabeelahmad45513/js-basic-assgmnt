be9f1a1cabbb3943b555f819cd07e63cd3277f11
nabeel <nabeelahmad45513>
2020-06-15 21:25:15 +0500

    js basic assgmnt

---
+++ app.js
@@ -0,0 +1,113 @@
+// ******* chapter # 1 task # 1 ***********//
+
+//alert("Asalam walikum");
+
+// ******* chapter # 1 task # 2 ***********//
+
+//document.write(" <H1> Hellow World </H1>");
+
+// ******* chapter # 1 task # 3 ***********//
+
+//document.write ("<h4>welcome to js land..</h4>  <h5>Happy Coding!</h5>");
+
+// ******* chapter # 1 task # 4 ***********//
+
+//alert("Welcome to js land...");
+
+// ******* chapter # 1 task # 5 ***********//
+
+//console.log("Hello.. ");
+
+// ******* chapter # 1 task # 6 ***********//
+
+
+// ******* chapter # 1 task # 7 ***********//
+
+//d. Body (after your page’s HTML);
+
+// ********* Chapter # 1 tak END 
+
+
+// ******* chapter # 2 task # 1 ***********//
+
+//prompt("User Name");
+
+// ******* chapter # 2 task # 2 ***********//
+
+// var a = "My Name is ";
+// var b = "Nabeel Ahmad";
+// var c = a+b;
+// alert(c)
+
+// ******* chapter # 2 task # 3 ***********//
+
+//prompt("Writte a message", "Hello World");
+
+// ******* chapter # 2 task # 4 ***********//
+
+//alert("21 years old");
+
+// ******* chapter # 2 task # 5 ***********//
+
+
+
+// ******* chapter # 2 task # 6 ***********//
+
+//prompt("Email", "example@example.com");
+
+// ******* chapter # 2 task # 7 ***********//
+
+//var a = "I am trying to learn from the book A smarter way to lern javacript";
+//alert(a)
+
+// ******* chapter # 2 task # 8 ***********//
+
+//document.write ("Yah! I can write HTML content through JavaScript")
+
+// ******* chapter # 2 task # 9 ***********//
+
+//var a = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+//alert(a)
+
+//****************chapter # 2 END *********//
+
+// ******* chapter # 3 task # 1 ***********//
+var age = +prompt("ENter Your Age")
+if (age > 5)
+{
+   alert("correct") 
+}
+else 
+{
+alert("incorrect")
+}
+
+// ******* chapter # 3 task # 2 ***********//
+
+
+// ******* chapter # 3 task # 3 ***********//
+
+
+// ******* chapter # 3 task # 4 ***********//
+
+
+// ******* chapter # 3 task # 5 ***********//
+
+
+// ******* chapter # 3 task # 6 ***********//
+
+
+
+// ******* chapter # 3 task # 7 ***********//
+
+
+
+// ******* chapter # 3 task # 8 ***********//
+
+
+
+// ******* chapter # 3 task # 9 ***********//
+
+
+
+// ******* chapter # 2 task # 9 ***********//
